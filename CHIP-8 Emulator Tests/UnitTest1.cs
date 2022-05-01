a292ecb3d38e66a8cf2dcc4a8b09c1476afdf958
yourzero <jmac@mcki.nl>
2022-05-01 09:35:26 -0700

    add unit tests project

---
+++ CHIP-8 Emulator Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CHIP_8_Emulator_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
